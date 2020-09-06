5094df1ef4dcc0f5dd2c00d59720250cdd99896f
Ondrej Karkoszka <karkoszkaondrej@gmail.com>
2020-09-06 18:49:35 +0200

    Initial commit

---
+++ bin/dart_cheatsheet_codelab.dart
@@ -0,0 +1,5 @@
+import 'package:dart_cheatsheet_codelab/dart_cheatsheet_codelab.dart' as dart_cheatsheet_codelab;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_cheatsheet_codelab.calculate()}!');
+}
---
+++ test/dart_cheatsheet_codelab_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_cheatsheet_codelab/dart_cheatsheet_codelab.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
